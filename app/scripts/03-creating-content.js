5e21b0ff60a4bac0b7a66f0d29e6fc0fd7c9e4af
Quoc bui <q.bui@caesar.nl>
2016-08-28 19:34:42 +0200

    add editings cards

--- app/scripts/03-creating-content.js
+++ app/scripts/03-creating-content.js
@@ -20,7 +20,7 @@
     var saveNewList = placeholderCreateList.querySelector('.todo__card-list--save');
     var todoWrapper = placeholderCreateList.parentElement; // get parent element
 
-    function createList(){
+    function createList(event){
         var newList = document.createElement('ul');
         var newListName = placeholderListInput.value;
         var newListContent = '<li class="todo__card-list__tittle">' + newListName + '</li><li class="todo__card-item--create"><div class="todo__card-content"><input type="text" placeholder="add card tittle" class="placeholder__tittle-input" ><textarea placeholder="add card description" class="placeholder__description-area"></textarea></div><div class="todo__card-actions"><button class="btn__action todo__card-item--add">Add New Card</button><button class="btn__action todo__card-item--save">Save Card</button></div></li>'
@@ -35,12 +35,27 @@
 
     function createCard(event){
         var placeholderCreateCard = getAncestor(event.target, 'todo__card-item--create');
-        var newCardName = placeholderCreateCard.querySelector('.placeholder__tittle-input');
-        var newCardDescription = placeholderCreateCard.querySelector('.placeholder__description-area');
         placeholderCreateCard.classList.add('editing');
-        newCardName.focus();
+        placeholderCreateCard.querySelector('.placeholder__tittle-input').focus();
+    }
 
-        console.log(newCardName);
+    function saveCard(event){
+        var editingCard = getAncestor(event.target, 'todo__card-item--create');
+        var list = editingCard.parentElement;
+        // Get card content value input
+        var cardTittle = editingCard.querySelector('.placeholder__tittle-input').value;
+        var cardDescription = editingCard.querySelector('.placeholder__description-area').value;
+        // Create new card content
+        var newCard = document.createElement('li');
+        var newCardContent = '<div class="todo__card-content"><h2 class="todo__card-tittle">' + cardTittle + '</h2><p class="todo__card-description">' + cardDescription + '</p></div>'
+        newCard.setAttribute('class', 'todo__card-item');
+        newCard.innerHTML = newCardContent;
+        // // Insert card
+        list.insertBefore(newCard, editingCard);
+        //reset .create-card element
+        editingCard.classList.remove('editing');
+        editingCard.querySelector('.placeholder__tittle-input').value = '';
+        editingCard.querySelector('.placeholder__description-area').value = '';
     }
 
     // When click add list > show input & auto focus + save button
@@ -54,10 +69,14 @@
         createList();
     });
 
-    // When click add card > show title & description input + save button
     todoWrapper.addEventListener('click', function(event){
+        // When click add card > show title & description input
         if(event.target.classList.contains('todo__card-item--add')) {
-           createCard(event);
+            createCard(event);
+
+        // When click save card > insert a new card
+        } else if(event.target.classList.contains('todo__card-item--save')){
+            saveCard(event);
         }
     });
 
